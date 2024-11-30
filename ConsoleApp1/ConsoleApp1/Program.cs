f9de6a7de7f0d63442548bc8e86d1ea05b33470c
s18324 <s18324@pjwstk.edu.pl>
2024-11-30 10:39:41 +0100

    Metoda liczenia średniej

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        double average = Math.CalculateAverage(numbers);
 
-Console.WriteLine("APBD! Modyfikacja 3");
+        Console.WriteLine($"Średnia wynosi: {average}");
+    }
+}
+
+public class Math
+{
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Tablica nie może być pusta.");
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        return (double)sum / numbers.Length;
+    }
+}
