1a47b932514596740cb995298763a30e4dc52497
s18324 <s18324@pjwstk.edu.pl>
2024-11-30 10:45:18 +0100

    Wyliczanie max

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,6 +6,9 @@
         double average = Math.CalculateAverage(numbers);
 
         Console.WriteLine($"Średnia wynosi: {average}");
+        
+        int maxValue = Math.FindMaxValue(numbers);
+        Console.WriteLine($"Maksymalna wartość wynosi: {maxValue}");
     }
 }
 
@@ -25,5 +28,24 @@ public class Math
         }
 
         return (double)sum / numbers.Length;
+    }
+    
+    public static int FindMaxValue(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Tablica nie może być pusta.");
+        }
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        return max;
     }
 }
